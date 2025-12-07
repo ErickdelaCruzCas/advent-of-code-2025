3dd337febfc341b2810e4a8d784658e8043d7d56
ErickdelaCruzCas <erickdlcruzcas@gmail.com>
2025-12-07 16:05:22 +0100

    feat: day 7 with cache

--- Day 7 Laboratories/my_solution_part2.py
+++ Day 7 Laboratories/my_solution_part2.py
@@ -1,3 +1,6 @@
+from functools import cache
+
+
 f = open("input.txt", "r")
 filas = []
 for line in f:
@@ -6,21 +9,22 @@ f.close()
 anchoDatos = len(filas[0])
 altoDatos = len(filas)
 
-ramasCalculadas = {}
+# ramasCalculadas = {}
 
+@cache
 def laserPath(y, x):
   if(y == altoDatos-1):
     # caso base, fin del camino
     return 1
-  elif((y,x) in ramasCalculadas):
-    # usa el número de caminos previamente calculado desde este divisor
-    return ramasCalculadas[(y,x)]
+  # elif((y,x) in ramasCalculadas):
+  #   # usa el número de caminos previamente calculado desde este divisor
+  #   return ramasCalculadas[(y,x)]
   elif(filas[y][x] == "^"):
     # calcula los caminos desde este divisor
     output = 0
     output += laserPath(y+1, x-1)
     output += laserPath(y+1, x+1)
-    ramasCalculadas[(y,x)] = output
+    # ramasCalculadas[(y,x)] = output
     return output
   else:
     # el láser pasó por espacio vacío
