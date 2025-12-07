a4b2c45f10f249a4804ff8864d9c3e9d4601952c
ErickdelaCruzCas <erickdlcruzcas@gmail.com>
2025-12-07 11:52:36 +0100

    feat: day 6 part 1 most reduced

--- Day 6 Trash Compactor/my_solution.py
+++ Day 6 Trash Compactor/my_solution.py
@@ -1,30 +1,13 @@
+from functools import reduce
+
+
 print(f"Empezamos")
 res = 0
+matrix = []
 with open("input.txt", "r", encoding="utf-8") as f:
-    intervals = list()
-    phase = 1
-    for line in (l.strip() for l in f):
-        
-        # print(f"Line Processing: {line}")
-  
-        
-        if phase == 2:       
-            ingredient = int(line)
-            fresh = False
-            i = 0
-            while not fresh and i < len(intervals): 
-                if intervals[i][0] <= ingredient <= intervals[i][1]:
-                    res += 1 
-                    fresh = True   
-                i+=1
-              
-        if phase == 1:
-            if line != "":
-                a, b = line.split("-")
-                intervals.append((int(a), int(b)))
-            else:
-                phase = 2              
-        
-
-    
-print(f"Solución y valor de Cafeteria: {res}")        
+    matrix = [line.split() for line in f]
+    for i, operator in enumerate(matrix[-1]):
+        res += reduce(
+            lambda a, b: eval(f"{a}{operator}{b}"), 
+            [matrix[j][i] for j in range(len(matrix) - 1)])
+print(f"Solución y valor de Trash Compactor: {res}")        
